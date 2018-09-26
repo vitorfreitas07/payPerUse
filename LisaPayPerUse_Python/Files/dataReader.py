1adcb4fbb668bf057dc16697406f12604b3ad7a0
diogomfarinha <42933731+diogomfarinha@users.noreply.github.com>
2018-09-26 18:18:08 +0100

    Changes made do databases

--- LisaPayPerUse_Python/Files/dataReader.py
+++ LisaPayPerUse_Python/Files/dataReader.py
@@ -2,13 +2,18 @@ import pandas as pd
 
 #Load data
 df_products=pd.read_csv("../Data/produtos.txt",sep='|',encoding='ANSI',dtype=str)
-df_rent=pd.read_csv("../Data/produtos_rent.txt",sep='|',encoding='ANSI',dtype=str)
-df_historico=pd.read_csv("../Data/historico_Manuel.txt",sep='|',encoding='ANSI',dtype=str)
+df_historico=pd.read_csv("../Data/historico.txt",sep='|',encoding='ANSI',dtype=str)
+df_clientes=pd.read_csv("../Data/clientes.txt",sep='|',encoding='ANSI',dtype=str)
 
+#Set columns to proper types
+df_products['PRICE'] = df_products['PRICE'].astype(float)
+df_products['RENT_PRICE'] = df_products['RENT_PRICE'].astype(float)
+
+df_historico['QUANTITY']=df_historico['QUANTITY'].astype(float)
+df_historico['DISCOUNT']=df_historico['DISCOUNT'].astype(float)
 
 #Criar tabela toda com historico do Manuel
 df_historico_completo=df_historico.merge(df_products,on=['PRODUCT_ID'],how='left')
 
-
-
+#Possible Statistics
 
