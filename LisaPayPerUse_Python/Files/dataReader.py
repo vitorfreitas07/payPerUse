b100fb9821a6fccc62fb322283eb2aad4e7f8a8e
diogomfarinha <42933731+diogomfarinha@users.noreply.github.com>
2018-09-27 19:57:50 +0100

    graphs added

--- LisaPayPerUse_Python/Files/dataReader.py
+++ LisaPayPerUse_Python/Files/dataReader.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import datetime
 
 #Load data
 df_products=pd.read_csv("../Data/produtos.txt",sep='|',encoding='ANSI',dtype=str)
@@ -10,6 +11,12 @@ df_products['PRICE'] = df_products['PRICE'].astype(float)
 
 df_historico['QUANTITY']=df_historico['QUANTITY'].astype(float)
 df_historico['DISCOUNT']=df_historico['DISCOUNT'].astype(float)
+
+def convert_to_datetime(string):
+    date=datetime.datetime.strptime(string, "%d/%m/%Y")
+    return date
+
+df_historico['DATE']=df_historico.apply(lambda row: convert_to_datetime(row['DATE']),axis=1)
 
 #Criar tabela toda com historico do Manuel
 df_historico_completo=df_historico.merge(df_products,on=['PRODUCT_ID'],how='left')
@@ -29,4 +36,12 @@ df_food_only=df_historico_completo.loc[((df_historico_completo['TYPE']!='Action 
 df_food_only=df_food_only.dropna(subset=['TYPE'])
 df_sum_food= df_food_only.groupby('TYPE').TOTAL_MONEY.sum()
 df_sum_food.plot(x='TYPE',y='TOTAL_MONEY',kind='bar')
+
+#Money spent per month
+df_historico_completo['MONTH']=df_historico_completo.apply(lambda row: row['DATE'].month,axis=1)
+df_sum_month=df_historico_completo.groupby('MONTH').TOTAL_MONEY.sum()
+df_sum_month.plot(x='MONTH',y='TOTAL_MONEY',kind='bar')
+
+
+
 
