5ff7f1b36137988b42b8dcd6bd96b41e372f5209
vitorfreitas07 <vitorfreitas1995@outlook.pt>
2018-10-03 19:08:18 +0100

    most of server complete

--- Java_Server/src/com/company/Main.java
+++ Java_Server/src/com/company/Main.java
@@ -21,13 +21,16 @@ public class Main {
         HttpServer server = HttpServer.create(new InetSocketAddress(8004),0);
         server.createContext("/login", new loginHandler());
         server.createContext("/register", new registerHandler());
+        server.createContext("/categorias", new categoriasHandler());
+        server.createContext("/search", new searchHandler());
+        server.createContext("/catalogo", new catalogoHandler());
+        server.createContext("/historico", new historicoHandler());
+        //getHistorico();
+
 
         //addClient("joao", "lolada", "1234567");
         //System.out.println("joao" + " has registered in the system!");
 
-
-        String result = "";
-        //listProducts();
 
         server.setExecutor(null); // creates a default executor
         server.start();
@@ -63,6 +66,64 @@ public class Main {
 
             Thread thread = new registerThread(t);
 
+            thread.start();
+
+        }
+
+    }
+
+    static class categoriasHandler implements HttpHandler {
+        // Handle incoming calls in threads
+
+        @Override
+
+        public void handle(HttpExchange t) throws IOException {
+
+            Thread thread = new categoriasThread(t);
+
+            thread.start();
+
+        }
+
+    }
+
+    static class searchHandler implements HttpHandler {
+        // Handle incoming calls in threads
+
+        @Override
+
+        public void handle(HttpExchange t) throws IOException {
+
+            Thread thread = new searchThread(t);
+
+            thread.start();
+
+        }
+
+    }
+
+
+    static class catalogoHandler implements HttpHandler {
+        // Handle incoming calls in threads
+
+        @Override
+
+        public void handle(HttpExchange t) throws IOException {
+
+            Thread thread = new catalogoThread(t);
+            thread.start();
+
+        }
+
+    }
+
+    static class historicoHandler implements HttpHandler {
+        // Handle incoming calls in threads
+
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+
+            Thread thread = new historicoThread(t);
             thread.start();
 
         }
@@ -209,16 +270,210 @@ public class Main {
         }
     }
 
+    static class categoriasThread extends Thread
+    {
+
+        private String username, password;
+        private HttpExchange t;
+        private String query;
+
+        public categoriasThread(HttpExchange t)
+        {
+            //System.out.println("teste1");
+            this.t = t;
+            query = t.getRequestURI().getQuery();
+            /*try
+            {
+                query = URLDecoder.decode(query, "UTF-8");
+            }
+            catch(UnsupportedEncodingException e)
+            {
+                throw new IllegalStateException("Unsupported Encoding exception: ", e);
+            }*/
+        }
+
+        @Override
+        public void run()
+        {
+            String response = "";
+            if(query != null){
+                try {
+                    if(!getCategorias().equals("")){
+                        response = getCategorias();
+                        response = URLEncoder.encode(response, "UTF-8");
+                    }else{
+                        response = "0";
+                    }
+
+                    t.sendResponseHeaders(200, response.length());
+                    OutputStream os = t.getResponseBody();
+                    os.write(response.getBytes());
+                    os.close();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    static class searchThread extends Thread
+    {
+
+        private String username, password;
+        private HttpExchange t;
+        private String query;
+
+        public searchThread(HttpExchange t)
+        {
+            this.t = t;
+            query = t.getRequestURI().getQuery();
+            try
+            {
+                query = URLDecoder.decode(query, "UTF-8");
+            }
+            catch(UnsupportedEncodingException e)
+            {
+                throw new IllegalStateException("Unsupported Encoding exception: ", e);
+            }
+        }
+
+        @Override
+        public void run()
+        {
+            String response = "";
+            if(query != null){
+                try {
+                    response = searchProduct(query);
+                    if(response.equals("")){
+                        response = URLEncoder.encode(response, "UTF-8");
+                    }else{
+                        response = "0";
+                    }
+
+                    t.sendResponseHeaders(200, response.length());
+                    OutputStream os = t.getResponseBody();
+                    os.write(response.getBytes());
+                    os.close();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+
+    static class catalogoThread extends Thread
+    {
+
+        private String username, password;
+        private HttpExchange t;
+        private String query;
+
+        public catalogoThread(HttpExchange t)
+        {
+            //System.out.println("teste1");
+            this.t = t;
+            query = t.getRequestURI().getQuery();
+            try
+            {
+                query = URLDecoder.decode(query, "UTF-8");
+            }
+            catch(UnsupportedEncodingException e)
+            {
+                throw new IllegalStateException("Unsupported Encoding exception: ", e);
+            }
+        }
+
+        @Override
+        public void run()
+        {
+            String response = "";
+            if(query != null){
+                try {
+                    if(!getCatalogo().equals("")){
+                        response = getCatalogo();
+                        response = URLEncoder.encode(response, "UTF-8");
+                    }else{
+                        response = "0";
+                    }
+
+                    t.sendResponseHeaders(200, response.length());
+                    OutputStream os = t.getResponseBody();
+                    os.write(response.getBytes());
+                    os.close();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    static class historicoThread extends Thread
+    {
+
+        private String username, password;
+        private HttpExchange t;
+        private String query;
+
+        public historicoThread(HttpExchange t)
+        {
+            //System.out.println("teste1");
+            this.t = t;
+            query = t.getRequestURI().getQuery();
+            try
+            {
+                query = URLDecoder.decode(query, "UTF-8");
+            }
+            catch(UnsupportedEncodingException e)
+            {
+                throw new IllegalStateException("Unsupported Encoding exception: ", e);
+            }
+        }
+
+        @Override
+        public void run()
+        {
+            String response = "";
+            if(query != null){
+                try {
+                    if(!getHistorico().equals("")){
+                        response = getHistorico();
+                        response = URLEncoder.encode(response, "UTF-8");
+                    }else{
+                        response = "0";
+                    }
+
+                    t.sendResponseHeaders(200, response.length());
+                    OutputStream os = t.getResponseBody();
+                    os.write(response.getBytes());
+                    os.close();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 
 
 
     public static int verifyClient(String username, String password)
     {
+
         int result = 0;
         try {
             Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/academydb", "root", "");
 
-            String query = "SELECT COUNT(*) as num FROM `clientes` WHERE name = '"+username+"' AND password ='"+password+"'";
+            String query = "SELECT COUNT(*) as num FROM `clientes` WHERE email = '"+username+"' AND password ='"+password+"'";
 
             // create the java statement
             Statement st = connection.createStatement();
@@ -252,11 +507,17 @@ public class Main {
     public static void addClient(String username, String password, String nif)
     {
         int result = 0;
+        String query = "";
         try
         {
             Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/academydb", "root", "");
-            // Verify if user already exists
-            String query = "INSERT INTO `clientes`(`name`, `password`, `nif`) VALUES ('"+username+"','"+password+"','"+nif+"')";
+
+
+                if(nif == null){
+                    query = "INSERT INTO clientes(email, password) VALUES ('"+username+"','"+password+"')";
+                }else{
+                    query = "INSERT INTO clientes(email, password, nif) VALUES ('"+username+"','"+password+"','"+nif+"')";
+                }
 
 
             // create the java statement
@@ -274,64 +535,69 @@ public class Main {
         }
     }
 
-
-    public static int listProducts()
+    public static String getCategorias()
     {
-        ArrayList<String> names = new ArrayList<>();
-        ArrayList<String> prices=new ArrayList<>();
-
-        try {
+        int result = 0;
+        String response = "";
+        try
+        {
             Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/academydb", "root", "");
+            // Verify if user already exists
+            String query = "";//"INSERT INTO `clientes`(`name`, `password`, `nif`) VALUES ('"+username+"','"+password+"','"+nif+"')";
 
-            String query = "SELECT Name, Price from produtos";
 
             // create the java statement
             Statement st = connection.createStatement();
 
             // execute the query, and get a java resultset
-            ResultSet rs = st.executeQuery(query);
+            st.executeQuery(query);
 
+            // execute the query, and get a java resultset
+            ResultSet rs = st.executeQuery(query);
 
             // Verify if client exists
             while(rs.next()){
-                names.add(rs.getString("name"));
-                prices.add(rs.getString("price"));
+                response = response + rs.getString("type");
                 //System.out.println(result);
             }
 
             st.close();
             connection.close();
-
-            if(!names.isEmpty())
-            {
-                for(int i = 0; i < names.size();i++){
-                    //System.out.println(names.get(i)+" "+prices.get(i));
-                }
-                return 1;
-            }
         }
         catch (SQLException e)
         {
             throw new IllegalStateException("Database exception: ", e);
         }
-        return 0;
+
+        return response;
     }
 
 
 
-    public static int listarHistorico()
+
+
+    public static String getCatalogo()
     {
+        //criar listas para os varios atributos do produto procurado
         ArrayList<String> names = new ArrayList<>();
-        //ArrayList<String> prices=new ArrayList<>();
-        ArrayList<String> prices=new ArrayList<>();
+        ArrayList<String> prices = new ArrayList<>();
+        ArrayList<String> type = new ArrayList<>();
+        ArrayList<String> aisle = new ArrayList<>();
+        ArrayList<String> shelf = new ArrayList<>();
 
-        try {
+        String response = "";
+        try
+        {
             Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/academydb", "root", "");
+            // Verify if user already exists
+            String query = "Select Name, Price,Type,Aisle,Shelf from produtos left join stocks on stocks.product_id=produtos.product_id order by Name asc";
 
-            String query = "SELECT Name, Price from produtos"; //historico..
 
             // create the java statement
             Statement st = connection.createStatement();
+
+            // execute the query, and get a java resultset
+            st.executeQuery(query);
 
             // execute the query, and get a java resultset
             ResultSet rs = st.executeQuery(query);
@@ -339,26 +605,148 @@ public class Main {
             // Verify if client exists
             while(rs.next()){
                 names.add(rs.getString("name"));
-                prices.add(rs.getString("price"));
+                type.add(rs.getString("type"));
+                aisle.add(rs.getString("aisle"));
+                shelf.add(rs.getString("shelf"));
                 //System.out.println(result);
             }
 
-            st.close();
-            connection.close();
 
-            if(!names.isEmpty())
-            {
-                for(String name: names ){
-                    //System.out.println(name);
-                }
-                return 1;
+            for(int i = 0; i < names.size();i++){
+                response = response + names.get(i)+ "," + type.get(i)+ "," + aisle.get(i) + "," + shelf.get(i)+"&";
             }
+
+            if (response != null && response.length() > 0 && response.charAt(response.length() - 1) == '&') {
+                response = response.substring(0, response.length() - 1);
+            }
+            System.out.println(response);
+
+
+            //response = response +;
+                    st.close();
+            connection.close();
         }
         catch (SQLException e)
         {
             throw new IllegalStateException("Database exception: ", e);
         }
-        return 0;
+
+        return response;
+    }
+
+
+    public static String getHistorico()
+    {
+        //criar listas para os varios atributos do produto procurado
+        ArrayList<String> dates = new ArrayList<>();
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<String> quantities = new ArrayList<>();
+        ArrayList<String> total = new ArrayList<>();
+
+        String response = "";
+        try
+        {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/academydb", "root", "");
+            // Verify if user already exists
+            String query = "Select Date,Name,Quantity,round(Quantity*Price*(1-Discount),2) as Total from produtos LEFT JOIN historico ON historico.product_id = produtos.product_id WHERE Date is not null ORDER BY date desc";
+
+
+            // create the java statement
+            Statement st = connection.createStatement();
+
+            // execute the query, and get a java resultset
+            st.executeQuery(query);
+
+            // execute the query, and get a java resultset
+            ResultSet rs = st.executeQuery(query);
+
+            // Verify if client exists
+            while(rs.next()){
+                dates.add(rs.getString("date"));
+                names.add(rs.getString("name"));
+                quantities.add(rs.getString("quantity"));
+                total.add(rs.getString("total"));
+                //System.out.println(result);
+            }
+
+
+            for(int i = 0; i < names.size();i++){
+                response = response + dates.get(i)+ "," + names.get(i) + "," + quantities.get(i)+ "," + total.get(i) + "&";
+            }
+
+            if (response != null && response.length() > 0 && response.charAt(response.length() - 1) == '&') {
+                response = response.substring(0, response.length() - 1);
+            }
+            System.out.println(response);
+
+
+            //response = response +;
+            st.close();
+            connection.close();
+        }
+        catch (SQLException e)
+        {
+            throw new IllegalStateException("Database exception: ", e);
+        }
+
+        return response;
+    }
+
+
+    public static String searchProduct(String toBeSearched)
+    {
+        //criar listas para os varios atributos do produto procurado
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<String> prices = new ArrayList<>();
+        ArrayList<String> brand = new ArrayList<>();
+
+        String response = "";
+        try
+        {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/academydb", "root", "");
+            // Verify if user already exists
+            String query = "select name, price, brand from produtos where name like '%"+ toBeSearched +"%'";
+
+
+            // create the java statement
+            Statement st = connection.createStatement();
+
+            // execute the query, and get a java resultset
+            st.executeQuery(query);
+
+            // execute the query, and get a java resultset
+            ResultSet rs = st.executeQuery(query);
+
+            // Verify if client exists
+            while(rs.next()){
+
+                names.add(rs.getString("name"));
+                prices.add(rs.getString("price"));
+                brand.add(rs.getString("brand"));
+                //System.out.println(result);
+            }
+
+
+            for(int i = 0; i < names.size();i++){
+                response = response + names.get(i)+ "," + prices.get(i) + "," + brand.get(i) + "&";
+            }
+
+            if (response != null && response.length() > 0 && response.charAt(response.length() - 1) == '&') {
+                response = response.substring(0, response.length() - 1);
+            }
+            System.out.println(response);
+
+
+            //response = response +;
+            st.close();
+            connection.close();
+        }
+        catch (SQLException e)
+        {
+            throw new IllegalStateException("Database exception: ", e);
+        }
+
+        return response;
     }
 
 
