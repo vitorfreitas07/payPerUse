3713b2313bc0ce34d7f135102e8669f30ab33f72
Alexandre Pereira <alexandre.figueiredo.pereira@tecnico.ulisboa.pt>
2018-10-03 18:00:47 +0100

    ANDROID:cc_fields
    Zombie knows a new spot to land

---
+++ LisaPayPerUse_Android/app/src/main/java/com/example/nbacademy/lisapayperuse/CreditcardActivity.java
@@ -0,0 +1,13 @@
+package com.example.nbacademy.lisapayperuse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreditcardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creditcard);
+    }
+}
